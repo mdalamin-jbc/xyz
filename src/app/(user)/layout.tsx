671376bf2bb83f4bd548aff33ed881c6ac7ab55c
mdalamin-jbc <jbc.alaminhaque@gmail.com>
2025-07-02 10:37:10 +0600

    [fix]: Sidebar problem

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -1,9 +1,8 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useSubscriptionGuard } from "@/hooks/payment/useSubscriptionGuard";
-import { user_role } from "@/constants/role";
 import Sidebar from "@/components/dashboard/user/Sidebar";
 import MenuProfile from "@/components/dashboard/user/MenuProfile";
 
@@ -13,28 +12,42 @@ interface Props {
 
 const UserDashboardLayout = ({ children }: Props) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
-
   const { loading, hasActiveSubscription } = useSubscriptionGuard(true);
+
+  const pathname = usePathname();
+
+  // Define routes where sidebar and menu should be hidden
+  const hideUIRoutes = ["/login", "/register", "/user/verify-otp"];
+  const shouldHideUI = hideUIRoutes.includes(pathname);
 
   const handleMenuToggle = () => setSidebarOpen(!sidebarOpen);
   const handleSidebarClose = () => setSidebarOpen(false);
 
   return (
     <div className="main_gradient_bg min-h-screen text-white flex">
-      {/* Sidebar (toggle works on mobile) */}
-      <Sidebar isOpen={sidebarOpen} onClose={handleSidebarClose} />
+      {!shouldHideUI && (
+        <Sidebar isOpen={sidebarOpen} onClose={handleSidebarClose} />
+      )}
 
-      {/* Main Content */}
-      <div className="flex-1 lg:ml-[250px] h-screen flex flex-col">
-        <div className="fixed top-0 left-0 lg:left-[250px] right-0 z-30">
-          <MenuProfile
-            text="ユーザーダッシュボード"
-            onMenuToggle={handleMenuToggle}
-          />
-        </div>
+      <div
+        className={`flex-1 ${
+          !shouldHideUI ? "lg:ml-[250px]" : ""
+        } h-screen flex flex-col`}
+      >
+        {!shouldHideUI && (
+          <div className="fixed top-0 left-0 lg:left-[250px] right-0 z-30">
+            <MenuProfile
+              text="ユーザーダッシュボード"
+              onMenuToggle={handleMenuToggle}
+            />
+          </div>
+        )}
 
-        {/* Scrollable children */}
-        <main className="mt-[80px] overflow-y-auto h-full px-4">
+        <main
+          className={`mt-[${
+            !shouldHideUI ? "80px" : "0"
+          }] overflow-y-auto h-full px-4`}
+        >
           {children}
         </main>
       </div>
